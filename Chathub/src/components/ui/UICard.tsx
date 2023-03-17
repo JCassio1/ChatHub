b70ed81f16d1c59788d7117529a08564acf1347a
JCassio1 <Joselsondias@hotmail.com>
2023-03-17 21:56:57 +0000

    Add props to cards

--- Chathub/src/components/ui/UICard.tsx
+++ Chathub/src/components/ui/UICard.tsx
@@ -1,11 +1,16 @@
-const UICard = () => {
+interface CardProps {
+  cardTitle: string
+  cardSubheading: string
+}
+
+const UICard = ({ cardTitle, cardSubheading }: CardProps): JSX.Element => {
   return (
     <div className='rounded-2xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1 shadow-xl'>
       <a className='block rounded-xl bg-white p-4 sm:p-6 lg:p-8' href=''>
         <div className='h-80 w-60'>
-          <h3 className='text-lg font-bold text-gray-900 sm:text-xl'>Science of Chats</h3>
+          <h3 className='text-lg font-bold text-gray-900 sm:text-xl'>{cardTitle}</h3>
 
-          <p className='mt-2 text-sm text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p className='mt-2 text-sm text-gray-500'>{cardSubheading}</p>
         </div>
       </a>
     </div>
--- Chathub/src/pages/Homepage/components/CardSection.tsx
+++ Chathub/src/pages/Homepage/components/CardSection.tsx
@@ -5,9 +5,9 @@ const CardSection = () => {
     <div className='flex items-center justify-center'>
       <div className='flex flex-col md:flex-row'>
         <div className='flex flex-col md:flex-row gap-5 md:gap-20 items-center justify-center overflow-hidden cursor-pointer'>
-          <UICard />
-          <UICard />
-          <UICard />
+          <UICard cardTitle='Write' cardSubheading='Share intimate and funny moments with friends or family.' />
+          <UICard cardTitle='Preview' cardSubheading='View before you send.' />
+          <UICard cardTitle='Send' cardSubheading='Sending to everyone you know.' />
         </div>
       </div>
     </div>
