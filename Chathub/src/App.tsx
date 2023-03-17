0990997f65d6838080cea32d5b427c45c4e58d21
JCassio1 <Joselsondias@hotmail.com>
2023-03-17 19:45:10 +0000

    Add Hero Section

--- Chathub/src/App.tsx
+++ Chathub/src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import Homepage from './pages/Homepage/Homepage'
 
 function App() {
   return (
     <div className='App'>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <Homepage />
     </div>
   )
 }
---
+++ Chathub/src/pages/Homepage/Homepage.tsx
@@ -0,0 +1,15 @@
+import Navbar from '../../components/layout/Navbar'
+import HeroSection from './components/HeroSection'
+
+const Homepage = () => {
+  return (
+    <>
+      <section>
+        <Navbar />
+        <HeroSection />
+      </section>
+    </>
+  )
+}
+
+export default Homepage
