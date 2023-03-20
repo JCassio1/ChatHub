407f1bab9d87e3df4915d9d2fd44f36db319be82
JCassio1 <Joselsondias@hotmail.com>
2023-03-20 23:59:06 +0000

    Add logo to branding sections

--- Chathub/src/pages/Homepage/components/BrandsSection.tsx
+++ Chathub/src/pages/Homepage/components/BrandsSection.tsx
@@ -1,11 +1,15 @@
+import AppleLogo from '../../../assets/apple-256.png'
+import Meta from '../../../assets/meta.png'
+import Amazon from '../../../assets/amazon.png'
+
 const BrandsSection = () => {
   return (
     <section>
       <div className='bg-red-800 py-8'>
         <div className='container mx-auto flex flex-wrap justify-around items-center'>
-          <img className='h-8 mr-6 mb-4' src='/images/netflix-white.png' alt='Netflix' />
-          <img className='h-8 mr-6 mb-4' src='/images/apple-white.png' alt='Apple' />
-          <img className='h-8 mr-6 mb-4' src='/images/nike-white.png' alt='Nike' />
+          <img className='h-16 mr-6 mb-4' src={AppleLogo} alt='Apple' />
+          <img className='h-16 mr-6 mb-4' src={Meta} alt='Meta' />
+          <img className='h-16 mr-6 mb-4' src={Amazon} alt='Amazon' />
         </div>
       </div>
     </section>
