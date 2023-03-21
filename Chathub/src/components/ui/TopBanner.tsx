88de2ea7fe04fa438671897dd580f7627f9975b2
JCassio1 <Joselsondias@hotmail.com>
2023-03-21 23:31:46 +0000

    Add top banner

---
+++ Chathub/src/components/ui/TopBanner.tsx
@@ -0,0 +1,13 @@
+interface banner {
+  bannerText: string
+}
+
+const TopBanner = ({ bannerText }: banner) => {
+  return (
+    <div className='bg-indigo-600 px-4 py-3 text-white'>
+      <p className='text-center text-sm font-medium'>{bannerText}</p>
+    </div>
+  )
+}
+
+export default TopBanner
