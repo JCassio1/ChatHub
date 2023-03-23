e0c983cac06cb30ea7173148ac45d47f3c871f89
JCassio1 <Joselsondias@hotmail.com>
2023-03-23 14:19:53 +0000

    Add image to cards and improve text

--- Chathub/src/components/ui/UICard.tsx
+++ Chathub/src/components/ui/UICard.tsx
@@ -6,13 +6,21 @@ interface CardProps {
 const UICard = ({ cardTitle, cardSubheading }: CardProps): JSX.Element => {
   return (
     <div className='rounded-2xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1 shadow-xl'>
-      <a className='block rounded-xl bg-white p-4 sm:p-6 lg:p-8' href=''>
+      <div className='block rounded-xl bg-white p-4 sm:p-6 lg:p-8'>
         <div className='h-80 w-60'>
-          <h3 className='text-lg font-bold text-gray-900 sm:text-xl'>{cardTitle}</h3>
+          <div className='relative flex mb-3 h-32 w-full justify-center rounded-xl bg-cover'>
+            <img
+              src='https://horizon-tailwind-react-git-tailwind-components-horizon-ui.vercel.app/static/media/Nft3.3b3e6a4b3ada7618de6c.png'
+              className='absolute flex h-32 w-full justify-center rounded-xl bg-cover'
+            />
+          </div>
+          <span className='whitespace-nowrap mt-20 rounded-full bg-purple-100 px-2.5 text-base text-purple-700'>
+            {cardTitle}
+          </span>
 
-          <p className='mt-2 text-sm text-gray-500'>{cardSubheading}</p>
+          <p className='mt-4 text-xl font-normal text-neutral-900'>{cardSubheading}</p>
         </div>
-      </a>
+      </div>
     </div>
   )
 }
--- Chathub/src/pages/Homepage/components/CardSection.tsx
+++ Chathub/src/pages/Homepage/components/CardSection.tsx
@@ -5,9 +5,9 @@ const CardSection = () => {
     <div className='flex items-center justify-center' style={{ zIndex: '0' }}>
       <div className='flex flex-col md:flex-row'>
         <div className='flex flex-col md:flex-row gap-5 md:gap-20 items-center justify-center overflow-hidden cursor-pointer'>
-          <UICard cardTitle='Write' cardSubheading='Share intimate and funny moments with friends or family.' />
-          <UICard cardTitle='Preview' cardSubheading='View before you send.' />
-          <UICard cardTitle='Send' cardSubheading='Sending to everyone you know.' />
+          <UICard cardTitle='Write' cardSubheading='Prepare the moments to share.' />
+          <UICard cardTitle='Preview' cardSubheading='Include every tiny detail of the story' />
+          <UICard cardTitle='Send' cardSubheading='Share with friends or family.' />
         </div>
       </div>
     </div>
