1c6b1cb2078e245b804a69ff6244930c6c817896
JCassio1 <Joselsondias@hotmail.com>
2023-03-23 16:43:37 +0000

    Add react router

--- Chathub/src/App.tsx
+++ Chathub/src/App.tsx
@@ -1,9 +1,17 @@
 import Homepage from './pages/Homepage/Homepage'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Homepage />
+  }
+])
 
 function App() {
   return (
     <div className='App'>
-      <Homepage />
+      <RouterProvider router={router} />
     </div>
   )
 }
