87810617287b680dfc5ac37fe7c399421b847df6
JCassio1 <Joselsondias@hotmail.com>
2023-04-10 12:51:21 +0100

    Update context to allow profile update

--- Chathub/src/hooks/AuthContext.tsx
+++ Chathub/src/hooks/AuthContext.tsx
@@ -4,18 +4,25 @@ import {
   signInWithPopup,
   User,
   UserCredential,
-  signInWithEmailAndPassword
+  signInWithEmailAndPassword,
+  updateProfile
 } from 'firebase/auth'
 import { auth, googleProvider } from '../config/firebase'
 import { Props } from '../Model/data-structures'
 import LoadingScreen from '../components/ui/LoadingScreen'
+import { getRandomAvatarUrl } from '../utils/helpers'
 
 type Context = {
   currentUser: User | null
-  createAccount: (email: string, password: string) => null | ReturnType<typeof createUserWithEmailAndPassword>
+  createAccount: (
+    email: string,
+    password: string,
+    displayName: string
+  ) => null | ReturnType<typeof createUserWithEmailAndPassword>
   signInWithFirebase: (email: string, password: string) => null | ReturnType<typeof signInWithEmailAndPassword>
   signInWithGoogle: () => null | ReturnType<typeof signInWithEmailAndPassword>
   logoutWithFirebase: () => null | ReturnType<typeof auth.signOut>
+  updateProfileWithDisplayName: (user: User | null, displayName: string) => null | ReturnType<typeof updateProfile>
 }
 
 export const AuthContext = React.createContext<Context>({
@@ -23,7 +30,8 @@ export const AuthContext = React.createContext<Context>({
   createAccount: () => null,
   signInWithFirebase: () => null,
   signInWithGoogle: () => null,
-  logoutWithFirebase: () => null
+  logoutWithFirebase: () => null,
+  updateProfileWithDisplayName: () => null
 })
 
 export function useAuth() {
@@ -34,8 +42,13 @@ export const AuthProvider = ({ children }: Props) => {
   const [currentUser, setCurrentUser] = useState<User | null>(null)
   const [loading, setLoading] = useState(true)
 
-  const createAccount = (email: string, password: string) => {
-    return createUserWithEmailAndPassword(auth, email, password)
+  const createAccount = async (email: string, password: string, displayName: string) => {
+    return createUserWithEmailAndPassword(auth, email, password).then(async (userCredential) => {
+      const user = userCredential.user
+      return updateProfileWithDisplayName(user, displayName).then(() => {
+        return userCredential
+      })
+    })
   }
 
   const signInWithFirebase = (email: string, password: string) => {
@@ -48,6 +61,13 @@ export const AuthProvider = ({ children }: Props) => {
 
   const logoutWithFirebase = () => {
     return auth.signOut()
+  }
+
+  const updateProfileWithDisplayName = (user: User, displayName: string) => {
+    return updateProfile(user, {
+      displayName: displayName,
+      photoURL: getRandomAvatarUrl()
+    })
   }
 
   useEffect(() => {
@@ -68,7 +88,8 @@ export const AuthProvider = ({ children }: Props) => {
     createAccount,
     signInWithGoogle,
     signInWithFirebase,
-    logoutWithFirebase
+    logoutWithFirebase,
+    updateProfileWithDisplayName
   }
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
