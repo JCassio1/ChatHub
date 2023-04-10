07718dfc338a543f57e06dd11b1f6870fe56d6fc
JCassio1 <Joselsondias@hotmail.com>
2023-04-10 12:52:04 +0100

    New accounts will be assigned avatar

---
+++ Chathub/src/utils/avatarData.tsx
@@ -0,0 +1,13 @@
+export const avatarArray = [
+  'https://api.dicebear.com/6.x/avataaars-neutral/svg?seed=Simon',
+  'https://api.dicebear.com/6.x/avataaars-neutral/svg?seed=Pepper',
+  'https://api.dicebear.com/6.x/avataaars-neutral/svg?seed=Bear',
+  'https://api.dicebear.com/6.x/avataaars-neutral/svg?seed=Peanut',
+  'https://api.dicebear.com/6.x/avataaars-neutral/svg?seed=Rascal',
+  'https://api.dicebear.com/6.x/avataaars-neutral/svg?seed=Bob',
+  'https://api.dicebear.com/6.x/avataaars-neutral/svg?seed=Snuggles',
+  'https://api.dicebear.com/6.x/big-ears-neutral/svg?seed=Princess',
+  'https://api.dicebear.com/6.x/big-ears-neutral/svg?seed=Snuggles',
+  'https://api.dicebear.com/6.x/fun-emoji/svg?seed=Bailey',
+  'https://api.dicebear.com/6.x/fun-emoji/svg?seed=Angel'
+]
--- Chathub/src/utils/helpers.tsx
+++ Chathub/src/utils/helpers.tsx
@@ -1,5 +1,15 @@
+import { avatarArray } from './avatarData'
+
 export function validateEmail(email: string): boolean {
   const res =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   return res.test(String(email).toLowerCase())
+}
+
+export function getRandomInt(max: number): number {
+  return Math.floor(Math.random() * max)
+}
+
+export function getRandomAvatarUrl(): string {
+  return avatarArray[getRandomInt(12)]
 }
