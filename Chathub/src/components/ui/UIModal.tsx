36c53d7f585b2af2cd55ce10aacba1a106a13647
JCassio1 <Joselsondias@hotmail.com>
2023-04-23 22:43:38 +0100

    Remove unnecessary tags

--- Chathub/src/components/ui/UIModal.tsx
+++ Chathub/src/components/ui/UIModal.tsx
@@ -9,20 +9,14 @@ const UIModal = ({ isOpen, onClose, title, body, buttonText, isButtonDisabled, h
       {isOpen && (
         <div className='fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none'>
           <div className='relative w-auto max-w-md mx-auto my-6'>
-            {/* Overlay */}
             <div className='fixed inset-0 bg-gray-500 opacity-75 transition-opacity' onClick={handleClose}></div>
 
-            {/* Content */}
             <div className='relative bg-white w-full rounded-lg shadow-lg'>
-              {/* Header */}
               <div className='p-5 text-center border-b border-gray-300'>
                 <h3 className='text-xl font-bold leading-tight text-gray-900'>{title}</h3>
               </div>
-
-              {/* Body */}
               <div className='p-5'>{body}</div>
 
-              {/* Footer */}
               <div className='flex justify-center items-center px-5 py-3 border-t jus border-gray-300'>
                 <button
                   disabled={isButtonDisabled}
