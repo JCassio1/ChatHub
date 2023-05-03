c2d86d04d44ef5baf549796d81cbdf5a3729a4a4
JCassio1 <Joselsondias@hotmail.com>
2023-05-03 23:36:37 +0100

    Fix: Environment variables

--- Chathub/src/config/firebase.ts
+++ Chathub/src/config/firebase.ts
@@ -19,9 +19,9 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
 
-  authDomain: process.env.AUTH_DOMAIN,
-
   projectId: process.env.PROJECT_ID,
+
+  authDomain: process.env.AUTH_DOMAIN,
 
   storageBucket: process.env.STORAGE_BUCKET,
 
